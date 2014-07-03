9c4f97492bbfc4ac59a89f0ea5df97c713d1682e
Alex Rice <agr@ncgr.org>
2014-07-03 12:42:33 -0600

    implemented default view which appears on tripals' Search Data page.

--- theme/templates/tripal_phylotree_base.tpl.php
+++ theme/templates/tripal_phylotree_base.tpl.php
@@ -2,15 +2,14 @@
    $node->content (created via _node_view) -->
 <div class="tripal_phylotree-data-block-desc tripal-data-block-desc"></div>
 
-<?php
-
-/* this template depends on d3js, but i am not putting it into
-tripal_phylotree.info scripts[] because that results in the script
-getting loaded *on every drupal request*! */
-drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/d3/3.4.8/d3.min.js','external');
-drupal_add_js( path_to_theme() . '/js/d3.phylogram.js' );
-
-?>
+<script>
+ <?php
+ // write a js var having URL of json data source for charting
+ $phylotree = $variables['node']->phylotree; 
+ print sprintf('var phylogramJsonUrl = "?q=chado_phylotree/%d/json";',
+               $phylotree->phylotree_id );
+ ?>
+</script>
 
 <div id="phylogram">
    <!-- d3js will add svg to this div, and remove the loader gif -->
@@ -19,39 +18,18 @@ drupal_add_js( path_to_theme() . '/js/d3.phylogram.js' );
 </div>
 
 <p>Phylogenies are essential to any analysis of evolutionary gene
-  sequences collected across a group of
-  organisms. A <strong>phylogram</strong> is a phylogenetic tree that
-  has branch spans proportional to the amount of character change.
+  sequences collected across a group of organisms. A <b>phylogram</b>
+  is a phylogenetic tree that has branch spans proportional to the
+  amount of character change.
 </p>
-  
-<script>
- <?php
- $phylotree = $variables['node']->phylotree; 
- print sprintf('var phylogramJsonUrl = "?q=chado_phylotree/%d/json";',
-               $phylotree->phylotree_id );
- ?>
- 
- (function ($) {
-   // drupal7 jquery outer context
-   // use jquery $() here...
 
-   $(document).ready( function () {
-     $.getJSON( phylogramJsonUrl, function(data) {
-       
-       d3.phylogram.build('#phylogram', data, {
-	 width: 550,
-	 height: 400
-       });
+<?php
+/* this template depends on d3js, but i am not putting it into
+tripal_phylotree.info scripts[] because that results in the script
+getting loaded *on every drupal request*! */
+drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/d3/3.4.8/d3.min.js','external');
 
-       d3.phylogram.buildRadial('#phylotree-radial-graph', data, {
-	 width: 550
-       });
-
-       $('.phylogram-ajax-loader').remove();       
-     });
-  // jquery ready     
-   });
-   // drupal jquery context
-})(jQuery);
- 
-</script>
+drupal_add_js( path_to_theme() . '/js/d3.phylogram.js' );
+drupal_add_js( path_to_theme() . '/js/organism-bubble-plot.js' );
+drupal_add_js( path_to_theme() . '/js/phylotree.js' );
+?>
--- theme/templates/tripal_phylotree_teaser.tpl.php
+++ theme/templates/tripal_phylotree_teaser.tpl.php
@@ -1,13 +1,13 @@
 <?php
 $node = $variables['node'];
-$phylotree = $variables['node']->phylotree; ?>
+$phylotree = $node->phylotree;
+?>
 
 <div class="tripal_phylotree-teaser tripal-teaser"> 
   <div class="tripal-phylotree-teaser-title tripal-teaser-title"><?php 
     print l($node->title, "node/$node->nid", array('html' => TRUE));?>
   </div>
-  <div class="tripal-phylotree-teaser-text tripal-teaser-text"><?php 
-    print $node->name;
-    print "... " . l("[more]", "node/$node->nid"); ?>
+  <div class="tripal-phylotree-teaser-text tripal-teaser-text">
+    [teaser.tpl...]
   </div>
 </div>
