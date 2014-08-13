5dce18e7322b8337e31401b14588fb6e5d5d63b8
Alex Rice <agr@ncgr.org>
2014-08-13 11:22:27 -0600

    fixed SQL and refactored query out of teaser.tpl. removed unused hooks from node.inc

--- theme/templates/tripal_phylotree_teaser.tpl.php
+++ theme/templates/tripal_phylotree_teaser.tpl.php
@@ -1,42 +1,21 @@
 <?php
-/* create a short description of the phylotree: list of organism
-  abbreviations, number of features, this kind of thing. */
-
+drupal_add_css( path_to_theme() . '/theme/css/teaser-fix.css');
 $node = $variables['node'];
 $phylotree = $node->phylotree;
-drupal_add_css( path_to_theme() . '/theme/css/teaser-fix.css');
-
-$sql = 'SELECT n.phylonode_id, n.parent_phylonode_id, n.label AS name, '.
-       'n.distance AS length, f.feature_id, f.name AS feature_name, '.
-       'cvt.name AS cvterm_name, cvt.definition AS cvterm_definition, '.
-       'o.organism_id, o.abbreviation, o.common_name, o.genus, o.species '.
-       'FROM chado.phylonode n '.
-       'LEFT OUTER JOIN chado.cvterm cvt ON n.type_id = cvt.cvterm_id '.
-       'LEFT OUTER JOIN chado.feature f ON n.feature_id = f.feature_id ' .
-       'LEFT OUTER JOIN chado.organism o on f.organism_id = o.organism_id '.
-       'WHERE n.phylotree_id = :phylotree_id ' .
-       'AND n.feature_id IS NOT NULL';
-$args = array(':phylotree_id' => $phylotree->phylotree_id);
-$result = chado_query($sql, $args);
-
-// count the organisms and features in the result set
-$featureCount = 0;
-$organisms = array();
-foreach($result as $r) {
-  $featureCount++;
-  $organisms[ $r->abbreviation ] = true;
-}
+$summary = phylotree_feature_summary($phylotree->phylotree_id);
+$feature_count = 0;
 ?>
 <div class="tripal_phylotree-teaser tripal-teaser"> 
   <div class="tripal-phylotree-teaser-title tripal-teaser-title"><?php 
     print l($node->title, "node/$node->nid", array('html' => TRUE));?>
   </div>
   <div class="tripal-phylotree-teaser-text tripal-teaser-text">
-    <?php 
-     foreach($organisms as $key => $val) {
-      echo $key, ' ';
-     }
-     ?>
-    (<?php echo $featureCount ?> features)
+    <?php
+    foreach($summary as $organism_abbrev => $count) {
+      echo $organism_abbrev, ' ';
+      ++$feature_count;
+    }
+    ?>
+    (<?php echo $feature_count ?> features)
   </div>
 </div>
