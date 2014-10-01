ffd9b15a5079e6053fc5296c9877ec809204f636
Andrew Farmer <adf@ncgr.org>
2014-10-01 15:33:14 -0500

    display accession, and hyperlink it iff the dbxref urlprefix is given

--- theme/templates/tripal_phylotree_references.tpl.php
+++ theme/templates/tripal_phylotree_references.tpl.php
@@ -6,17 +6,19 @@
 $node = $variables['node'];
 $phylotree = $node->phylotree;
 
-$header = array('Database', 'Accession', 'Link Out');
+$header = array('Database', 'Accession');
+
+//not really sure about this, but leaving as alex had it for now
+$accession = sprintf('%s %s %s',
+      $phylotree->dbxref_id->accession,
+      $phylotree->dbxref_id->version,
+      $phylotree->dbxref_id->description);
 
 if($phylotree->dbxref_id->db_id->url) {
-  $url = sprintf('%s%s%s',
+  $url = sprintf('%s%s',
       $phylotree->dbxref_id->db_id->urlprefix,
-      $phylotree->dbxref_id->db_id->url,
-      $phylotree->dbxref_id->accession);
-  $link_out = sprintf('<a href="%s" target="_blank">%s</a>', $url, $url);
-}
-else {
-  $link_out = 'n/a';
+      $accession);
+  $linkout = sprintf('<a href="%s" target="_blank">%s</a>', $url, $accession);
 }
 
 $rows = array(
@@ -24,11 +26,7 @@ $rows = array(
     sprintf('%s %s',
      $phylotree->dbxref_id->db_id->name,
      $phylotree->dbxref_id->db_id->description),
-    sprintf('%s %s %s',
-      $phylotree->dbxref_id->accession,
-      $phylotree->dbxref_id->version,
-      $phylotree->dbxref_id->description),
-    $link_out
+    ($linkout ? $linkout : $accession)
   )));
 
 $table = array(
