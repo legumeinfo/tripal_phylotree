2582addc1d3d23ff48198e26732b2c1ae6a45c2b
Iliana Toneva <itoneva@ncgr.org>
2015-03-10 16:41:07 -0500

    lis specific links removed from js; code indent fixed

--- theme/js/tripal_phylotree.js
+++ theme/js/tripal_phylotree.js
@@ -10,12 +10,12 @@
   
   $(document).ready( function () {
     
-    var legumeColors = null;
+    var colors = null;
 
     // function to generate color based on the organism genus and species
     // on graph node d
     var organismColor = function(d) {
-      var color = legumeColors[d.genus + ' ' + d.species];
+      var color = colors[d.genus + ' ' + d.species];
       if(color) { return color; }
       else { return 'grey'; }
     };
@@ -59,39 +59,18 @@
 
       if(d.children) {
         // interior node
-        if(d.phylonode_id) {
-          var link = $('#phylonode_context_link');
-	  //eventually, this link will be replaced with something internal to the site; note that the trailing slash is somewhat important to avoid apparent hanging due to the way django handles url pattern matching
-          link.attr('href', 'http://comparative-legumes.org/chado/context_viewer/' + d.phylonode_id + '/');
-          link.text('View Genomic Contexts for genes in this subtree');
-          link.show();
-        }
-        else {
-          // this shouldn't happen but ok
-          $('#phylonode_context_link').hide();
-        }
-        
-        // show dialog content relevant for interior node
-	// go_link not ready for prime time
-        // $('#phylonode_go_link').show();
-        $('#phylonode_go_link').hide();
-        $('#phylonode_context_link').show();
-        
+
         // hide dialog content which is only applicable to leaf nodes
         $('#phylonode_organism_link').hide();
         $('#phylonode_feature_link').hide();
-      }
+      } 
       else {
         // leaf node
 
         // show dialog content relevant for leaf node
         $('#phylonode_organism_link').show();
         $('#phylonode_feature_link').show();
-        
-        // hide dialog content which is only applicable to interior nodes
-        $('#phylonode_go_link').hide();
-        $('#phylonode_context_link').hide();
-        
+	  
         if(d.feature_node_id) {
           var link = $('#phylonode_feature_link');
           link.attr('href', '?q=node/' + d.feature_node_id);
@@ -100,55 +79,7 @@
 
         }
         else {
-          // this shouldn't happen but ok
           $('#phylonode_feature_link').hide();
-        }
-	if (d.feature_name) {
-          var link = $('#phylonode_gene_linkout');
-          //FIXME: hack depending on typical naming conventions. we can certainly do better
-          var transcript = d.feature_name.replace(/^.....\./, "");
-          var gene = transcript.replace(/\.\d+$/, "");
-          if (d.genus == 'Glycine' && d.species == 'max') {
-                  link.attr('href', 'http://www.soybase.org/sbt/search/search_results.php?category=FeatureName&search_term=' + gene);
-                  //link.attr('href', 'http://www.soybase.org/gb2/gbrowse/gmax2.0/?q=' + gene + ';dbid=gene_models_wm82_a2_v1');
-                  link.text('view at SoyBase: ' + gene);
-                  link.show();
-          }
-          else if (d.genus == 'Phaseolus' && d.species == 'vulgaris') {
-                  link.attr('href', 'http://phytozome.jgi.doe.gov/pz/portal.html#!gene?organism=Pvulgaris&searchText=locusName:' + gene);
-                  link.text('view at Phytozome: ' + gene);
-                  link.show();
-          }
-          else if (d.genus == 'Medicago' && d.species == 'truncatula') {
-                  link.attr('href', 'http://medicago.jcvi.org/medicago/jbrowse/?data=data%2Fjson%2Fmedicago&loc='+transcript+'&tracks=DNA%2Cgene_models&highlight=');
-                  link.text('view at JCVI: ' + transcript);
-                  link.show();
-          }
-          else if (d.genus == 'Arachis' && d.species == 'duranensis') {
-                  //link.attr('href', 'http://peanutbase.org/gb2/gbrowse/Aradu1.0/?q=' + gene);
-                  //link.attr('href', 'http://peanutbase-stage.agron.iastate.edu/gb2/gbrowse/Aradu1.0/?q=' + gene);
-                  link.attr('href', 'http://peanutbase.org/gb2/gbrowse/Aradu1.0/?q=' + gene + ';dbid=gene_models');
-                  //link.attr('href', 'http://peanutbase-stage.agron.iastate.edu/gb2/gbrowse/Aradu1.0/?q=' + gene + ';dbid=gene_models');
-                  link.text('view at PeanutBase: ' + gene);
-                  link.show();
-          }
-          else if (d.genus == 'Arachis' && d.species == 'ipaensis') {
-                  //link.attr('href', 'http://peanutbase.org/gb2/gbrowse/Araip1.0/?q=' + gene);
-                  //link.attr('href', 'http://peanutbase-stage.agron.iastate.edu/gb2/gbrowse/Araip1.0/?q=' + gene);
-                  link.attr('href', 'http://peanutbase.org/gb2/gbrowse/Araip1.0/?q=' + gene + ';dbid=gene_models');
-                  //link.attr('href', 'http://peanutbase-stage.agron.iastate.edu/gb2/gbrowse/Araip1.0/?q=' + gene + ';dbid=gene_models');
-                  link.text('view at PeanutBase: ' + gene);
-                  link.show();
-          }
-          else if (d.genus == 'Arabidopsis' && d.species == 'thaliana') {
-                  link.attr('href', ' http://www.araport.org/locus/' + gene);
-                  link.text('view at Arabidopsis Information Portal: ' + gene);
-                  link.show();
-          }
-          else {
-                  link.hide();
-          }
-        
         }
         if(d.organism_id) {
           var link = $('#phylonode_organism_link');
@@ -161,7 +92,7 @@
           // there should always be an organism id, but degrade gracefully
           $('#phylonode_organism_link').hide();
         }
-      }
+	}
     
       dialog.dialog( {
         title : title,
@@ -172,8 +103,8 @@
       });
     };
 
-    $.getJSON(pathToTheme +'/js/legume-colors.json', function(colorData) {
-      legumeColors = colorData;
+    $.getJSON(pathToTheme +'/js/colors.json', function(colorData) {
+      colors = colorData;
       $.getJSON(phylotreeDataURL, function(treeData) {
         displayData(treeData);
         $('.phylogram-ajax-loader').remove();
