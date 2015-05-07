1a9d2ad7e5b79bc8e49e4a7dc655a49cfacb371a
Andrew Farmer <adf@ncgr.org>
2015-05-07 10:50:13 -0500

    added tree name and description

--- theme/templates/tripal_phylotree_radial.tpl.php
+++ theme/templates/tripal_phylotree_radial.tpl.php
@@ -1,3 +1,15 @@
+<div class="tripal_phylotree-data-block-desc tripal-data-block-desc">
+<?php
+$phylotree = $variables['node']->phylotree;
+print $phylotree->name;
+?>
+<br/>
+<?php
+if( ! empty($phylotree->comment) ) {
+  print $phylotree->comment;
+}
+?>
+</div>
 
 <div id="phylotree-radial-graph">
   <!-- d3 will add svg to this div -->
