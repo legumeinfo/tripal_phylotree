73ddd360b741c25b7822bbdb95d41808bea43347
sdash-github <sdash@ncgr.org>
2015-05-22 18:12:55 -0500

    SDash: Userinfo block and content created for Phylotree module.

---
+++ theme/templates/tripal_phylotree_userinfo.tpl.php
@@ -0,0 +1,28 @@
+<!-- This is a test page under development -->
+<!-- Current URL alias in lis-dev: /tutorial/helpfiles/geneFamilySearchHelp-->
+
+<h1>HOW TO: Gene Family Search</h1>
+<div style="font-family:times">
+<p>This page lets you search for gene families starting from a Phytozome gene family ID (e.g., phytozome_10_2.59192412) or a word from the description of the gene (gene name or function), e.g., iron homeostasis or chlorophyll binding protein, etc.  You may leave any of the search fields blank if you don't care about the field. The gene families are based on <a href="http://phytozome.jgi.doe.gov/pz/portal.html" target="_blank">Phytozome version 10.2</a>.  You can further refine or restrict the search results by choosing the count fields under different species (the maximum number of members in a particular species).</p>
+
+<p>The resulting table shows the Family ID and description along with the membership counts in different species. The Family ID links to a page displaying a phylogram of all its members (a phylogenetic tree that has branch spans proportional to the amount of character change) , both legumes and non-legumes, and helps reveal the extent of relationship among them. Clicking a legume gene in the phylogram will lead to more details about the gene at LIS including its sequence (clicking a non-legume member is currently inert).  There is choice to display the results as a circular dendrogram, membership counts in different taxa, and few other details as shown in the side panel.</p>
+
+<p>Click an internal node to view the genomic contexts for genes in this subtree.</p>
+
+<p>This tools can help you lead to very related genes, based on sequence similarity for further exploration, in legumes and other plant species.</p>
+
+</div>
+
+<!--SCRATCH PAD-->
+<!--
+Ð which version; usage of forms for species count may not be obvious to naive users;
+
+Others that should appear in the help contents:
+Ð What happens when family id is clicked, total count is clicked and spp. count is clicked.
+Ð Mention that it goes to make a basket of the list and from there what can you do with links.
+Ð Source of the data on which this phylotree/family tool operates
+Ð 2 different simple/short use cases for illustrating benefit of this tool.
+-- clicking internal node (need more details)
+-->
+
+
