1cd97858f542300828d5f9f21da8206711d106d1
Alex Rice <agr@ncgr.org>
2016-08-08 18:02:29 -0500

    Port to lisTours bootstrap-tour.

--- theme/templates/tripal_phylotree_base.tpl.php
+++ theme/templates/tripal_phylotree_base.tpl.php
@@ -9,9 +9,6 @@ if(empty($my_path)) {
   $my_path = 'sites/all/modules/tripal/tripal_phylogeny';
 }
 drupal_add_css($my_path . '/theme/css/phylogram.css');
-drupal_add_css(
-    '//cdnjs.cloudflare.com/ajax/libs/hopscotch/0.2.5/css/hopscotch.min.css',
-    'external');
 
 $phylotree = $variables['node']->phylotree;
 ?>
@@ -70,21 +67,21 @@ if( ! empty($phylotree->comment) ) {
 </div>
 
 <div style="display: none" id="phylogeny-help-dlg">
-  <p style="font-size: 0.9rem" id="hopscotch-tour-link">
+  <p style="font-size: 0.9rem" id="tour-link">
     <a href="#"
-       onclick="showHopscotchTour(); jQuery('#phylogeny-help-dlg').dialog('close');"
+       onclick="lisTours.go('phylotree'); jQuery('#phylogeny-help-dlg').dialog('close');"
        style="text-decoration: underline">
     View Quick Tour
     </a>
   </p>
-    
+
   <p style="font-size: 0.9rem">
     <a href="/search/phylotree/userinfo" target="_blank"
     style="text-decoration: underline">
     View complete help on searching gene families...</a>
   </p>
 </div>
-    
+
 <div id="organism-legend-dialog" style="display: none">
   <!-- d3js will add content to this div -->
   <div class="organism-legend">
@@ -94,14 +91,14 @@ if( ! empty($phylotree->comment) ) {
 <div id="phylonode_popup_dialog" style="display: none;">
 </div>
 
-    
+
 <?php
-/* 
+/*
  * this template depends on a few javascript libraries, but i am not
  * putting it into tripal_phylotree.info scripts[] because that results
- * in the script getting loaded *on every drupal request* which is wasteful 
+ * in the script getting loaded *on every drupal request* which is wasteful
  */
-    
+
 drupal_add_js($my_path . '/theme/js/msa/dist/msa-1_0.min.js',
               array(
                   'type' => 'file',
@@ -118,11 +115,6 @@ drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/URI.js/1.18.1/URI.min.js',
                   'group' => JS_LIBRARY,
               ));
 drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/lodash.js/4.14.1/lodash.min.js',
-              array(
-                  'type' => 'external',
-                  'group' => JS_LIBRARY,
-              ));
-drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/hopscotch/0.2.5/js/hopscotch.min.js',
               array(
                   'type' => 'external',
                   'group' => JS_LIBRARY,
@@ -157,12 +149,12 @@ drupal_add_js('/'. $my_path . '/theme/js/phylotree.js',
                   'type' => 'file',
                   'group' => JS_DEFAULT,
               ));
-drupal_add_js('/'. $my_path . '/theme/js/tour.js',
+drupal_add_js('/'. $my_path . '/theme/js/phylogeny-msa.js',
               array(
                   'type' => 'file',
                   'group' => JS_DEFAULT,
               ));
-drupal_add_js('/'. $my_path . '/theme/js/phylogeny-msa.js',
+drupal_add_js('/'. $my_path . '/theme/js/tour.js',
               array(
                   'type' => 'file',
                   'group' => JS_DEFAULT,
