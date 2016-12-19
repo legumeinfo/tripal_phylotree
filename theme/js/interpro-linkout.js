001aacb07ccd79251320244aedff58a648a08e33
Alex Rice <agr@ncgr.org>
2016-12-19 16:25:56 -0600

    Remove Id from interpro linkout html element
    (could be duplicated if there is more than one IPR code)

--- theme/js/interpro-linkout.js
+++ theme/js/interpro-linkout.js
@@ -7,8 +7,8 @@ jQuery(document).ready( function($) {
 	var comment = commentEl.html();
 	var matches = comment.match(/IPR\d{6,6}/gi);
 	matches.forEach(function(code) {
-		var link = '<a id="interpro-help" href="'+
-					'http://www.ebi.ac.uk/interpro/entry/'+ code + '">'+code + '</a>';
+		var link = '<a href="http://www.ebi.ac.uk/interpro/entry/'+ code +
+				        '">'+code + '</a>';
 		comment = comment.replace(code, link);
 	});
 	commentEl.html(comment);
