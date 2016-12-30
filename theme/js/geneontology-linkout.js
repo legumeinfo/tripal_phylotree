10bce6570607752cd13e7acf2c5c296b9850602d
Andrew Farmer <adf@ncgr.org>
2016-12-30 14:36:36 -0600

    check for null after matching to avoid javascript errors

--- theme/js/geneontology-linkout.js
+++ theme/js/geneontology-linkout.js
@@ -6,10 +6,12 @@ jQuery(document).ready( function($) {
 	var commentEl = $('#phylotree-comment');
 	var comment = commentEl.html();
 	var matches = comment.match(/GO:\d{7,7}/gi);
-	matches.forEach(function(code) {
-		var link = '<a href="http://amigo.geneontology.org/amigo/term/'+ code +
-				        '">'+code + '</a>';
-		comment = comment.replace(code, link);
-	});
-	commentEl.html(comment);
+    if (matches != null) {
+        matches.forEach(function(code) {
+            var link = '<a href="http://amigo.geneontology.org/amigo/term/'+ code +
+                            '">'+code + '</a>';
+            comment = comment.replace(code, link);
+        });
+        commentEl.html(comment);
+    }
 });
