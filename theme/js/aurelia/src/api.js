f2096d21866c21729159fafbf2dcba27df8a7b4c
Andrew Farmer <adf@ncgr.org>
2019-05-11 21:22:08 -0500

    changes to support being able to install and build the module again. the crossfilter2 importing seems to have run afoul of changes occasioned by upgrades to gulp and use of babel preset-env, though as usual I walk away feeling like I am not completely satisfied with the resolution of the javascript modular morass

--- theme/js/aurelia/src/api.js
+++ theme/js/aurelia/src/api.js
@@ -6,9 +6,11 @@
 
 import {inject, observable} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-fetch-client';
-import * as crossfilter2 from 'crossfilter2';
-let crossfilter = crossfilter2.crossfilter;
-let fasta = msa.io.fasta; /* msa lib is loaded in <script> tag */
+
+import * as crossfilter from 'crossfilter2';
+
+let cf = crossfilter.default;
+let fasta = msa.io.fasta; 
 
 @inject(HttpClient)
 export class Api {
@@ -16,21 +18,21 @@ export class Api {
   TREE_URL = API.tree; // global var was defined in drupal template
   MSA_URL =  API.msa;  // global var was defined in drupal template
 
-	// the endpoint for the "family representative" links from the
-	// tripal_linkout module.
-	FAMREPS_LINKS_URL = '/famreps_links';
-	LEAF_LINKS_URL = '/phylotree_links/'; // genus//speciesnode.feature_name/json
+  // the endpoint for the "family representative" links from the
+  // tripal_linkout module.
+  FAMREPS_LINKS_URL = '/famreps_links';
+  LEAF_LINKS_URL = '/phylotree_links/'; // genus//speciesnode.feature_name/json
 
-	// declare some observable properties for use by other vis. elements:
-	@observable cf; // crossfilter object
-	@observable cfUpdated; // a crossfilter updated message
-	
+  // declare some observable properties for use by other vis. elements:
+  @observable cf; // crossfilter object
+  @observable cfUpdated; // a crossfilter updated message
+  
   treeData = null;
   msaSeqs = null;
-	loading = true;
+  loading = true;
   flatData = []; // same as tree leaves
   index = {};
-	
+  
   constructor(http) {
     this.http = http;
   }
@@ -41,43 +43,43 @@ export class Api {
     let p2 = this.getMsaData();
     // after both resources to load, then do more setup.
     let p3 = Promise.all([p1, p2]).then(() => {
-			this.loading = false;
+      this.loading = false;
       this.postProcess();
       this.setupCrossFilter();
     });
-		return p3;
+    return p3;
   }
-	
+  
   // by convention, the consensus seq is the first in the msa seqs array.
   getConsensusSeq() {
     return (this.msaSeqs[0].name.endsWith("-consensus") ? this.msaSeqs[0] : undefined);
   }
-	
+  
   setupCrossFilter() {
     // create crossfilter from flat data, not from tree data.
     let msg = null;
-		this.cf = crossfilter(this.flatData);
+    this.cf = cf(this.flatData);
   }
-	
+  
   getTreeData() {
-		if(treeData) {
-			// have global var from the php template
-			this.treeData = treeData;
-			this.parseTree(treeData);
-			return Promise.resolve(treeData);
-		}
-		else {
-			let promise = this.http.fetch(this.TREE_URL)
-					.then(res => res.json())
-					.then(data => {
-						this.treeData = data;
-						this.parseTree(data);
-						return this.treeData;
-					});
-			return promise;
-		}
+    if(treeData) {
+      // have global var from the php template
+      this.treeData = treeData;
+      this.parseTree(treeData);
+      return Promise.resolve(treeData);
+    }
+    else {
+      let promise = this.http.fetch(this.TREE_URL)
+          .then(res => res.json())
+          .then(data => {
+            this.treeData = data;
+            this.parseTree(data);
+            return this.treeData;
+          });
+      return promise;
+    }
   }
-	
+  
   getMsaData() {
     let promise = this.http.fetch(this.MSA_URL)
         .then(res => res.text())
@@ -88,7 +90,7 @@ export class Api {
         });
     return promise;
   }
-	
+  
   // postProcess() : add the corresponding MSA sequence to each record in the
   // flatData set.
   postProcess() {
@@ -102,12 +104,12 @@ export class Api {
       }
     });
   }
-	
+  
   // parseTree() : build an index of names, add properties as needed, and
   // create a flattened version of the tree.
   parseTree(node) {
     let that = this;
-		
+    
     if (! node.name) {
       node.name = '';
     }
@@ -129,32 +131,32 @@ export class Api {
     });
   }
 
-	getLinkouts(node) {
-		let d = node.data();
-		if(node.is_leaf() && !node.is_collapsed()) {
-			let url = `${this.LEAF_LINKS_URL}${d.genus}/${d.species}/${d.feature_name}/json`;
-			let promise = this.http.fetch(url)
-					.then(res => res.json());
-			return promise;
-		}
-		else {
-			// for an interior node, request the familty representatives.
-			// use POST because the number of features can exceed allowed
-			// URL length. note: this is a bit of a hack because PHP 5.6
-			// barfs when receiving a POST with application/json
-			// content-type. As a workaround, send x-www-form-urlencoded
-			// instead, and specify the body parameter.
-			let url = this.FAMREPS_LINKS_URL;
-			let query = 'famreps=' + node.get_all_leaves(true).map(n => n.data().feature_name).join(',');
-			let promise = this.http.fetch(url, {
-				method: 'post',
-				body: query,
-				headers: {
-					'content-type': 'application/x-www-form-urlencoded'
-				}
-			})
-					.then(res => res.json());
-			return promise;
-		}
-	}
+  getLinkouts(node) {
+    let d = node.data();
+    if(node.is_leaf() && !node.is_collapsed()) {
+      let url = `${this.LEAF_LINKS_URL}${d.genus}/${d.species}/${d.feature_name}/json`;
+      let promise = this.http.fetch(url)
+          .then(res => res.json());
+      return promise;
+    }
+    else {
+      // for an interior node, request the familty representatives.
+      // use POST because the number of features can exceed allowed
+      // URL length. note: this is a bit of a hack because PHP 5.6
+      // barfs when receiving a POST with application/json
+      // content-type. As a workaround, send x-www-form-urlencoded
+      // instead, and specify the body parameter.
+      let url = this.FAMREPS_LINKS_URL;
+      let query = 'famreps=' + node.get_all_leaves(true).map(n => n.data().feature_name).join(',');
+      let promise = this.http.fetch(url, {
+        method: 'post',
+        body: query,
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded'
+        }
+      })
+          .then(res => res.json());
+      return promise;
+    }
+  }
 }
